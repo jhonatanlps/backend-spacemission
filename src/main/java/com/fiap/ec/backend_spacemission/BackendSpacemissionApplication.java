0f4a87793aa7d7525bc4f18a0c16b6412ddb3a9e
Jhonatan <jhonatanlopessilva7@gmail.com>
2026-05-31 18:27:30 -0300

    chore: estrutura inicial gerada pelo Spring Initializr

---
+++ src/main/java/com/fiap/ec/backend_spacemission/BackendSpacemissionApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ec.backend_spacemission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpacemissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpacemissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/ec/backend_spacemission/BackendSpacemissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ec.backend_spacemission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpacemissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
