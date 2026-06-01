bbe17e8f25d516b6cef9e9744ec9850039e15275
Jhonatan <jhonatanlopessilva7@gmail.com>
2026-05-31 21:55:08 -0300

    feat: criação do repository do Alerta

---
+++ src/main/java/com/fiap/ec/backend_spacemission/repository/AlertaRepository.java
@@ -0,0 +1,7 @@
+package com.fiap.ec.backend_spacemission.repository;
+
+import com.fiap.ec.backend_spacemission.model.Alerta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlertaRepository extends JpaRepository<Alerta, Long> {
+}
